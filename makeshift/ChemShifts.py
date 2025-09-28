ceb3d183f4988a7ced8bdda1ca018fb1a285b0d6
Gina El Nesr <gelnesr@stanford.edu>
2025-09-28 08:16:28 -0700

    Check for valid input calculating CSI, add warnings

--- makeshift/ChemShifts.py
+++ makeshift/ChemShifts.py
@@ -1,8 +1,10 @@
-import pandas as pd
+import warnings
 import numpy as np
-from .parsing import convert_loop_to_dataframe
-from .ReRef import reref_panav_, reref_lacs_
+import pandas as pd
+
 from .utils.chemshift_utils import *
+from .ReRef import reref_panav_, reref_lacs_
+from .parsing import convert_loop_to_dataframe
 
 def clean_cs_dataframe(df):
     df['Val'] = df['Val'].replace('.',np.nan)
@@ -15,30 +17,33 @@ def get_chem_shifts(parsed, calc_CSI=False, reref=None):
 
     out_dfs=[]
     for k, entry in parsed['assigned_chemical_shifts'].items():
-      cs = convert_loop_to_dataframe(entry['_Atom_chem_shift'])
-      cs = clean_cs_dataframe(cs)
-      try:
-          cs['name'] = parsed[k]['Name']
-      except:
-          cs['name'] = '.'
-
-      cs['cs_saveframe_id'] = k
-      out_dfs.append(cs)
-    out = pd.concat(out_dfs)
+        cs = convert_loop_to_dataframe(entry['_Atom_chem_shift'])   
+        cs = clean_cs_dataframe(cs)
+        try:
+            cs['name'] = parsed[k]['Name']
+        except:
+            cs['name'] = '.'
+        cs['cs_saveframe_id'] = k
+        out_dfs.append(cs)
     
+    out = pd.concat(out_dfs)
+
     out['Seq_ID'] = out['Seq_ID'].astype(int)
     
-    if calc_CSI:
-
-        if 'CA' and 'CB' not in out['Atom_ID'].unique():
-            raise RuntimeError('Cannot find CA and CB in Atom_ID, cannot calc CSI')
-        
-        out['csi_raw'] = out.apply(lambda row: get_csi(row, out), axis=1)
-        out["csi"] = out.apply(lambda row: csi_index(row), axis=1).astype(float)
-
-    if reref=='panav':
+    if reref =='panav':
         out = reref_panav_(out)
-    elif reref=='lacs':
-        out = reref_lacs_(out)
+    elif reref =='lacs':
+        out = reref_lacs_(out)    
 
+    if calc_CSI:
+        if 'CA' and 'CB' not in out['Atom_ID'].unique():
+            warnings.warn("Cannot find CA and CB in Atom_ID. Cannot calculate CSI", UserWarning)
+        elif not sum(~pd.isna(out['Atom_ID'] == 'CA')):
+            warnings.warn("There are only np.nan values for CA. Cannot calculate CSI", UserWarning)
+        if not sum(~pd.isna(out['Atom_ID'] == 'CB')):
+            warnings.warn("There are only np.nan values for CB. Cannot calculate CSI", UserWarning)
+        else:
+            out['csi_raw'] = out.apply(lambda row: get_csi(row, out), axis=1)
+            out["csi"] = out.apply(lambda row: csi_index(row), axis=1).astype(float)
+    
     return out
